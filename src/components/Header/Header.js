19cb34dc8404ee897f94a6bef0923913b25ed1da
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-26 10:56:43 +0600

    add hero icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Bars4Icon } from '@heroicons/react/24/solid'
+
 
 const Header = () => {
     return (
-        <div>
-
-        </div>
+        <nav>
+            <div>
+                <Bars4Icon className='h-7 w-7'></Bars4Icon>
+            </div>
+            <ul className='md:flex '>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/men">Men</a></li>
+                <li><a href="/women">Women</a></li>
+                <li><a href="/child">Child</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
     );
 };
 
